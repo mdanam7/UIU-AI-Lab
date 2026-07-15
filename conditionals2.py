25d5495b74846b63547d6f01f1690d8126893c74
mdanam7 <itsanam777@gmail.com>
2026-07-15 11:21:27 +0600

    conditionals2.py

---
+++ conditionals2.py
@@ -0,0 +1,17 @@
+l=["Anam","Raihan","Rahat","Shafi","Dipu"]
+name=input("Enter your name: ")
+if(name in l):
+    print("You name is in the list.")
+else:
+    print("Sorry!Your name is not in the list")    
+
+
+
+#similar problem
+
+post=input("Enter the post: ")
+if("Abir".lower() in post.lower()):
+    print("This post is talking about Abir.")
+else:
+    print("THis post is not talking about Abir")    
+
